70b4d4ddf8d0cbb056ae4bcbbabc8a5ff9843dd6
bohcstams <b17_bohacs.tamas@vargaszolnok.hu>
2023-04-10 23:16:15 +0200

    v1.0 - Elvileg jó

---
+++ src/Character.java
@@ -0,0 +1,94 @@
+
+/**
+ * A karaktert reprezentáló osztály
+ */
+public class Character {
+    //A lépései száma az adott körben
+    private int moves;
+    //Az a mező, amin éppen áll
+    protected Field field;
+
+    /**
+     * Character konstruktor
+     * @param nMoves A lépések száma
+     * @param nField A mező, amelyen álljon
+     */
+    public Character(int nMoves, Field nField){
+        for(int i = 0; i < Main.tabCount; i++) {System.out.print("\t");}
+        Main.tabCount++;
+        moves = nmoves;
+        field = nfield;
+        System.out.println("Character has been initialized");
+        Main.tabCount--;
+    }
+
+    /**
+     * A lépések settere
+     * @param newMoves A lépések új értéke
+     */
+    public void setMoves(int newMoves){
+        for(int i = 0; i < Main.tabCount; i++) {System.out.print("\t");}
+        Main.tabCount++;
+        System.out.println("Character::setMoves()");
+        moves = newMoves;
+        Main.tabCount--;
+    }
+
+    /**
+     * A lépések gettere
+     */
+    public int getMoves(){
+        for(int i = 0; i < Main.tabCount; i++) {System.out.print("\t");}
+        Main.tabCount++;
+        System.out.println("Character::getMoves()");
+        return moves;
+        Main.tabCount--;
+    }
+
+    /**
+     * A jelenlegi mező settere
+     * @param newField A jelenlegi mező
+     */
+    public void setField(Field newField){
+        for(int i = 0; i < Main.tabCount; i++) {System.out.print("\t");}
+        Main.tabCount++;
+        System.out.println("Character::setField()");
+        field = newField;
+        Main.tabCount--;
+    }
+
+    /**
+     * A jelenlegi mező gettere
+     */
+    public Field getField(){
+        for(int i = 0; i < Main.tabCount; i++) {System.out.print("\t");}
+        Main.tabCount++;
+        System.out.println("Character::getField()");
+        return field;
+        Main.tabCount--;
+    }
+
+    /**
+     * Pumpa bemenetét és kimenetét állító függvény
+     * @param o Új bemenet
+     * @param d Új kimenet
+     */
+    public void ChangePump(Pipe o, Pipe d){
+        for(int i = 0; i < Main.tabCount; i++) {System.out.print("\t");}
+        Main.tabCount++;
+        System.out.println("Character::ChangePump()");
+        field.Change(o, d);
+    }
+
+    /**
+     * A mozgatást végző függvény
+     * @param d Egy irány aról a mezőről, amin áll
+     */
+    public void Move(int d){
+        Field neighbour = field.getNeighbour(d);
+        if(neighbour != null){
+            field.Remove(this);
+            neighbour.Accept(this);
+        }
+    }
+}
---
+++ src/Mechanic.java
@@ -0,0 +1,66 @@
+/**
+ * A szerelőt reprezentáló osztály
+ */
+public class Mechanic {
+    
+    private Pipe pipe_in_hand;
+    private Pump pump_in_hand;
+
+    /**
+     * Megkísérli megjavítani az adott mezőt, amin éppen áll függetlenül
+     * attól, hogy az cső vagy pumpa.
+     */
+    public void Fix(){
+        for(int i = 0; i < Main.tabCount; i++) {System.out.print("\t");}
+        Main.tabCount++;
+        System.out.println("Mechanic::Fix()");
+        field.Fix();
+        Main.tabCount--;
+    }
+
+    /**
+     * A szerelő leteszi a kezáben lévő pumpát, arra a csőre, amin áll
+     */
+    public void PlacePump(){
+        for(int i = 0; i < Main.tabCount; i++) {System.out.print("\t");}
+        Main.tabCount++;
+        System.out.println("Mechanic::PlacePump()");
+        pump_in_hand = field.PlacePump();
+        Main.tabCount--;
+    }
+
+    /**
+     * A szerelő beköti a kezében lévő csövet abba a pumpába, amin áll
+     */
+    public void PlacePipe(){
+        for(int i = 0; i < Main.tabCount; i++) {System.out.print("\t");}
+        Main.tabCount++;
+        System.out.println("Mechanic::PlacePipe()");
+        field.addEdge(pipe_in_hand);
+    }
+    /**
+     * A szerelőnek ezen függvénye hívódik meg, amikor a
+     * játékos egy ciszternában pumpát szeretne a kezébe venni. Ekkor felvesz egy
+     * pumpát, amivel a kezében mozoghat a pályán.
+     */
+    public void PickUpPump(){
+        for(int i = 0; i < Main.tabCount; i++) {System.out.print("\t");}
+        Main.tabCount++;
+        System.out.println("Mechanic::PickUpPump()");
+        pump_in_hand = field.PickUpPump();
+        Main.tabCount--;
+    }
+
+    /**
+     * A szerelő megkísérel felvenni egy csövet (pontosabban
+     * annak az egyik végét) arról a mezőről, amelyen áll, majd ha ez sikerül, a cső
+     * vége a kezébe kerül, ezzel tud tovább mozogni.
+     */
+    public void PickUpPipe(){
+        for(int i = 0; i < Main.tabCount; i++) {System.out.print("\t");}
+        Main.tabCount++;
+        System.out.println("Mechanic::PickUpPipe()");
+        pipe_in_hand = field.PickUpPipe();
+        Main.tabCount--;
+    }
+}
