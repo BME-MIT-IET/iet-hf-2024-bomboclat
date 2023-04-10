f00667aecb8b050dcf2593fd79da33b611507eee
Lili Nemes <lili.nemes@outlook.com>
2023-04-11 01:27:12 +0200

    Pump::PickUpPipe() kell, többi kész

--- src/Field.java
+++ src/Field.java
@@ -5,15 +5,20 @@ abstract public class Field{
     /**Az adott mezőn álló játékosok. */
     protected List<Character> players;
 
-    /**Field konstruktora */
+    /**Field konstruktora. */
     public Field(){
+        for(int i=0; i < Main.tabCount; i++) {
+            System.out.print("\t");
+        }
         System.out.println("Field has been initialized");
+        Main.tabCount++;
         players=new ArrayList<>();
+        Main.tabCount--;
     }
     
     /**
-     * Ez egy absztrakt függvény, mivel a leszármazottai más-más szabályok alapján adják meg, 
-     * hogy az adott irányba milyen szomszédos mezőjük van, illetve a leszármazottai tárolják a szomszédos
+     * Ez egy absztrakt függvény, mivel a Field osztály leszármazottai más-más szabályok alapján adják meg,
+     * hogy az adott irányba milyen szomszédos mezőjük van, illetve maguk a leszármazottak tárolják a szomszédos
      * mezőiket.
      * @param d Irány, amelyből a szomszédot lekérik.
      * @return Az adott irányból a szomszédos mező, vagy ha az adott irányba nincs érvényes mező, nullt ad vissza.
@@ -21,97 +26,133 @@ abstract public class Field{
     abstract public Field getNeighbour(int d);
 
     /**
-     * Leveszi a paraméterben kapott karaktert a mezőről.
+     * Leveszi a paraméterben kapott karaktert a mezőről, vagyis eltávolítja a players listából.
      * @param c Karakter objektum, amelyet le akarnak venni a mezőről.
      */
     public void Remove(Character c){
+        for(int i=0; i < Main.tabCount; i++) {
+            System.out.print("\t");
+        }
         System.out.println("Field::Remove()");
+        this.players.remove(c);
+        Main.tabCount++;
+        Main.tabCount--;
     }
 
     /**
-     * Felveszi a paraméterben kapott karaktert a mezőre.
+     * Felveszi a paraméterben kapott karaktert a mezőre, vagyis eltárolja a players listában.
      * @param c Karakter objektum, amelyet fel akarnak vetetni a mezőre.
      */
     public void Accept(Character c){
+        for(int i=0; i < Main.tabCount; i++) {
+            System.out.print("\t");
+        }
         System.out.println("Field::Accept()");
+        Main.tabCount++;
+        this.players.add(c);
         c.setField(this);
+        Main.tabCount--;
     }
 
     /**
      * Nem csinál semmit, elnyeli a hívást, ha olyan objektumra hívják, aminek nincsen ez a függvénye felüldefiniálva.
      */
     public void Fix(){
+        for(int i=0; i < Main.tabCount; i++) {
+            System.out.print("\t");
+        }
         System.out.println("Field::Fix()");
+        Main.tabCount++;
+        Main.tabCount--;
     }
 
     /**
      * Nem csinál semmit, elnyeli a hívást, ha olyan objektumra hívják, aminek nincsen ez a függvénye felüldefiniálva.
      */
     public void Drill(){
+        for(int i=0; i < Main.tabCount; i++) {
+            System.out.print("\t");
+        }
         System.out.println("Field::Drill()");
+        Main.tabCount++;
+        Main.tabCount--;
     }
 
     /**
      * Nem csinál semmit, elnyeli a hívást, ha olyan objektumra hívják, aminek nincsen ez a függvénye felüldefiniálva.
-     * @param p Az a cső, amelyet bementi csőként kell beállítani.
-     * @param d Az a cső, amelyet kimeneti csőként kell beállítani.
+     * @param in Az a cső, amelyet bementi csőként kell beállítani.
+     * @param out Az a cső, amelyet kimeneti csőként kell beállítani.
      */
-    public void Change(Pipe o, Pipe d){
+    public void Change(Pipe in, Pipe out){
+        for(int i=0; i < Main.tabCount; i++) {
+            System.out.print("\t");
+        }
         System.out.println("Field::Change()");
+        Main.tabCount++;
+        Main.tabCount--;
     }
 
     /**
      * Nem csinál semmit, elnyeli a hívást, ha olyan objektumra
      * hívják, aminek nincsen ez a függvénye felüldefiniálva.
-     * @return
+     * @return A Pipe objektum amit felvett, ha olyan objektumra hívják meg, melynek ez a függvénye felül van
+     * definiálva, ha olyanra aminek nincs akkor null.
      */
     public Pipe PickUpPipe(){
+        for(int i=0; i < Main.tabCount; i++) {
+            System.out.print("\t");
+        }
         System.out.println("Field::PickUpPipe()");
+        Main.tabCount++;
+        Main.tabCount--;
         return null;
     }
 
     /**
      * Nem csinál semmit, elnyeli a hívást, ha olyan objektumra
      * hívják, aminek nincsen ez a függvénye felüldefiniálva.
-     * @return 
+     * @return A Pump objektum amit felvett, ha olyan objektumra hívják meg, melynek ez a függvénye felül van
+     * definiálva, ha olyanra aminek nincs akkor null.
      */
     public Pump PickUpPump(){
+        for(int i=0; i < Main.tabCount; i++) {
+            System.out.print("\t");
+        }
         System.out.println("Field::PickUpPump()");
+        Main.tabCount++;
+        Main.tabCount--;
         return null;
     }
 
     /**
-     * 
-     */
-    public void PlacePipe(){
-        System.out.println("Field::PlacePipe()");
-    }
-
-    /**
-     * 
-     * @param d
-     * @return
-     */
-    public Node getEndpoint(int d){
-        System.out.println("Field::getEndpoint()");
-        return null;
-    }
-
-    /**
-     * 
-     * @param p
+     * Nem csinál semmit, elnyeli a hívást, ha olyan objektumra
+     * hívják, aminek nincsen ez a függvénye felüldefiniálva.
+     * @param p A lehelyezendő Pipe objektum.
      */
     public void addEdge(Pipe p){
+        for(int i=0; i < Main.tabCount; i++) {
+            System.out.print("\t");
+        }
         System.out.println("Field::addEdge()");
+        Main.tabCount++;
+        Main.tabCount--;
     }
 
     /**
-     * 
-     * @param p 
-     * @return
+     * Nem csinál semmit, elnyeli a hívást, ha olyan objektumra
+     * hívják, aminek nincsen ez a függvénye felüldefiniálva.
+     * @param p A lehelyezendő Pump objektum.
+     * @return Ha a pumpát le lehet helyezni az objektumra amin meghívják, mert felül van definiálva ez a
+     * függvénye, akkor null, egyéb esetben visszaadja a pumpát amit paraméterként kapott, megpróbált
+     * lehelyezni.
      */
     public Pump PlacePump(Pump p){
+        for(int i=0; i < Main.tabCount; i++) {
+            System.out.print("\t");
+        }
         System.out.println("Field::PlacePump()");
+        Main.tabCount++;
+        Main.tabCount--;
         return p;
     }
 
--- src/Node.java
+++ src/Node.java
@@ -5,10 +5,15 @@ abstract public class Node extends Field implements Stepable{
     /**A csomópontba befutó csövek. */
     protected List<Pipe> edges;
 
-    /**Node konstruktora */
+    /**Node konstruktora. */
     public Node(){
+        for(int i=0; i < Main.tabCount; i++) {
+            System.out.print("\t");
+        }
         System.out.println("Node has been initialized");
+        Main.tabCount++;
         edges=new ArrayList<>();
+        Main.tabCount--;
     }
 
     /**
@@ -17,7 +22,12 @@ abstract public class Node extends Field implements Stepable{
      * @return Az adott irányból a szomszédos mező, vagy ha az adott irányba nincs érvényes mező, nullt ad vissza.
      */
      public Field getNeighbour(int d){
+         for(int i=0; i < Main.tabCount; i++) {
+             System.out.print("\t");
+         }
         System.out.println("Node::getNeighbour()");
+         Main.tabCount++;
+         Main.tabCount--;
         return edges.get(d);
      }
 
@@ -28,30 +38,45 @@ abstract public class Node extends Field implements Stepable{
 
     /**
      * Összeszámolja a szerelők pontját a városba folyt 
-     * vízmennyiség alapján, ha városra hívódik meg, egyébként nem csinál semmit.
-     * @return Az összeadott pontok száma.
+     * vízmennyiség alapján, ha városra hívódik meg, egyébként nem csinál semmit, elnyeli a hívást.
+     * @return Az összeadott pontok száma, null ha olyan objektumra hívják meg ami nem definiálta felül a függvényt.
      */
     public int getMechanicPoint(){
+        for(int i=0; i < Main.tabCount; i++) {
+            System.out.print("\t");
+        }
         System.out.println("Node::getMechanicPoint()");
+        Main.tabCount++;
+        Main.tabCount--;
+        return null;
     }
 
     /**
-     * Hozzáaadja az adott csövet az edges listához (a szomszédaihoz).
-     * @param p Az adott cső.
+     * Hozzáaadja az paraméterként kapott csövet az edges listához (a szomszédaihoz).
+     * @param p Az paraméterként kapott cső.
      */
     public void addEdge(Pipe p){
+        for(int i=0; i < Main.tabCount; i++) {
+            System.out.print("\t");
+        }
         System.out.println("Node::addEdge()");
+        edges.add(p);
+        Main.tabCount++;
+        Main.tabCount--;
     }
 
     /**
-     * Eltávolítja az adott csövet az edges listából (a szomszédaiból).
-     * @param p Az adott cső.
+     * Eltávolítja a paraméterként kapott csövet az edges listából (a szomszédaiból).
+     * @param p A paraméterként kapott cső.
      */
     public void removeEdge(Pipe p){
+        for(int i=0; i < Main.tabCount; i++) {
+            System.out.print("\t");
+        }
         System.out.println("Node::removeEdge()");
+        edges.remove(p);
+        Main.tabCount++;
+        Main.tabCount--;
     }
-
-
-
 
 }
