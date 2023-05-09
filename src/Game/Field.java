eb5c1b2763bd55098bcea93c73fd32daaae5872e
esztertolm <eszter.tolmacsi@gmail.com>
2023-05-09 20:39:32 +0200

    Field osztály

--- src/Game/Field.java
+++ src/Game/Field.java
@@ -14,13 +14,7 @@ abstract public class Field{
 
     /**Field konstruktora. */
     public Field(){
-        for(int i=0; i < TesterMain.tabCount; i++) {
-            System.out.print("\t");
-        }
-        System.out.println("Field has been initialized");
-        TesterMain.tabCount++;
         players=new ArrayList<>();
-        TesterMain.tabCount--;
     }
     
     /**
@@ -37,13 +31,9 @@ abstract public class Field{
      * @param c Karakter objektum, amelyet le akarnak venni a mezőről.
      */
     public void Remove(Character c){
-        for(int i=0; i < TesterMain.tabCount; i++) {
-            System.out.print("\t");
+        if(c!=null){
+            players.remove(c);
         }
-        System.out.println("Field::Remove()");
-        this.players.remove(c);
-        TesterMain.tabCount++;
-        TesterMain.tabCount--;
     }
 
     /**
@@ -51,38 +41,22 @@ abstract public class Field{
      * @param c Karakter objektum, amelyet fel akarnak vetetni a mezőre.
      */
     public void Accept(Character c){
-        for(int i=0; i < TesterMain.tabCount; i++) {
-            System.out.print("\t");
+        if(c!=null){
+            this.players.add(c);
+            c.setField(this);
         }
-        System.out.println("Field::Accept()");
-        TesterMain.tabCount++;
-        this.players.add(c);
-        c.setField(this);
-        TesterMain.tabCount--;
     }
 
     /**
      * Nem csinál semmit, elnyeli a hívást, ha olyan objektumra hívják, aminek nincsen ez a függvénye felüldefiniálva.
      */
     public void Fix(){
-        for(int i=0; i < TesterMain.tabCount; i++) {
-            System.out.print("\t");
-        }
-        System.out.println("Field::Fix()");
-        TesterMain.tabCount++;
-        TesterMain.tabCount--;
     }
 
     /**
      * Nem csinál semmit, elnyeli a hívást, ha olyan objektumra hívják, aminek nincsen ez a függvénye felüldefiniálva.
      */
     public void Drill(){
-        for(int i=0; i < TesterMain.tabCount; i++) {
-            System.out.print("\t");
-        }
-        System.out.println("Field::Drill()");
-        TesterMain.tabCount++;
-        TesterMain.tabCount--;
     }
 
     /**
@@ -91,12 +65,6 @@ abstract public class Field{
      * @param out Az a cső, amelyet kimeneti csőként kell beállítani.
      */
     public void Change(Pipe in, Pipe out){
-        for(int i=0; i < TesterMain.tabCount; i++) {
-            System.out.print("\t");
-        }
-        System.out.println("Field::Change()");
-        TesterMain.tabCount++;
-        TesterMain.tabCount--;
     }
 
     /**
@@ -106,12 +74,6 @@ abstract public class Field{
      * definiálva, ha olyanra aminek nincs akkor null.
      */
     public Pipe PickUpPipe(Mechanic p){
-        for(int i=0; i < TesterMain.tabCount; i++) {
-            System.out.print("\t");
-        }
-        System.out.println("Field::PickUpPipe()");
-        TesterMain.tabCount++;
-        TesterMain.tabCount--;
         return null;
     }
 
@@ -122,12 +84,6 @@ abstract public class Field{
      * definiálva, ha olyanra aminek nincs akkor null.
      */
     public Pump PickUpPump(){
-        for(int i=0; i < TesterMain.tabCount; i++) {
-            System.out.print("\t");
-        }
-        System.out.println("Field::PickUpPump()");
-        TesterMain.tabCount++;
-        TesterMain.tabCount--;
         return null;
     }
 
@@ -137,12 +93,6 @@ abstract public class Field{
      * @param p A lehelyezendő Pipe objektum.
      */
     public void addEdge(Pipe p){
-        for(int i=0; i < TesterMain.tabCount; i++) {
-            System.out.print("\t");
-        }
-        System.out.println("Field::addEdge()");
-        TesterMain.tabCount++;
-        TesterMain.tabCount--;
     }
 
     /**
@@ -154,22 +104,19 @@ abstract public class Field{
      * lehelyezni.
      */
     public Pump PlacePump(Pump p){
-        for(int i=0; i < TesterMain.tabCount; i++) {
-            System.out.print("\t");
-        }
-        System.out.println("Field::PlacePump()");
-        TesterMain.tabCount++;
-        TesterMain.tabCount--;
         return p;
     }
     
+    /**
+     * Elnyeli a függvényhívást olyan mezőkre, ahol annak nincs funkciója.
+     */
     public void Glue(){
-
     }
 
+    /**
+     * Elnyeli a függvényhívást olyan mezőkre, ahol annak nincs funkciója.
+     */
     public void Lube(){
-
     }
-
 
 }
