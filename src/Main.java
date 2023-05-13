e46dd894f975cc932f32d1717093bcbb34c12843
KapszulasKaresz <nemethibence03@gmail.com>
2023-05-13 14:31:32 +0200

    hogy pörögjön a mainből

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 import Game.*;
 import skeleton.*;
+import CommandInterpreter.*;
 
 public class Main {
     public static void main(String[] args){
+        CommandInterpreter c = new CommandInterpreter();
+        c.main(args);
     }
 }
