e34e82f741eb34228aed9c719a4a04fde4dea236
esztertolm <eszter.tolmacsi@gmail.com>
2023-05-26 13:57:29 +0200

    .

--- src/Graphics/PipeView.java
+++ src/Graphics/PipeView.java
@@ -13,15 +13,19 @@ public class PipeView implements IView{
 
     public PipeView(Pipe _pipe){
         pipe = _pipe;
-        Node neighbour0 = pipe.getEndpoint(0);
-        Node neighbour1 = pipe.getEndpoint(1);
-        if(neighbour0!=null){
+        if(pipe.getEndpoint(0)!=null){
             end0X = pipe.getEndpoint(0).getView().getPlayerPositionX();
             end0Y = pipe.getEndpoint(0).getView().getPlayerPositionY();
+        }else{
+            end0X = -1;
+            end0Y = -1;
         }
-        if(neighbour1 != null){
+        if(pipe.getEndpoint(1)!= null){
             end1X = pipe.getEndpoint(1).getView().getPlayerPositionX();
             end1Y = pipe.getEndpoint(1).getView().getPlayerPositionY();
+        }else{
+            end1X = -1;
+            end1Y = -1;
         }
     }
 
@@ -33,31 +37,72 @@ public class PipeView implements IView{
         boolean isSlippery = pipe.getSlippery();
         boolean hasHole = pipe.getHasHole();
 
+        
+
+        if(pipe.getEndpoint(0)!=null){
+            end0X = pipe.getEndpoint(0).getView().getPlayerPositionX();
+            end0Y = pipe.getEndpoint(0).getView().getPlayerPositionY();
+        }else{
+            end0X = -1;
+            end0Y = -1;
+        }
+        if(pipe.getEndpoint(1)!= null){
+            end1X = pipe.getEndpoint(1).getView().getPlayerPositionX();
+            end1Y = pipe.getEndpoint(1).getView().getPlayerPositionY();
+        }else{
+            end1X = -1;
+            end1Y = -1;
+        }
+
+        // Graphics2D g2d = (Graphics2D)g;
+        // Rectangle rect1 = new Rectangle(100, 100, 20, 20);
+        // g2d.setColor(Color.WHITE);
+        // g2d.translate(rect1.x+(rect1.width/2), rect1.y+(rect1.height/2));
+        // g2d.rotate(Math.toRadians(90));
+        // g2d.draw(rect1);
+        // g2d.fill(rect1);
+
         if(!haswater){
             g.setColor(Color.BLACK);
         }else{
             g.setColor(Color.BLUE);
         }
-        
+        // Graphics2D g2d = (Graphics2D)g;
+        // Rectangle rect1 = new Rectangle(end0X, end0Y, , 20);
     }
 
     public Pipe getPipe(){
         return pipe;
     }
 
+    private int distanceBetweenTwoPoints(int x1, int x2, int y1, int y2){
+        return 1;
+    }
+
     public void setPipe(Pipe p){
         pipe = p;
     }
 
+    //TODO kezelni kell, ha nincs szomszédja
     @Override
     public int getPlayerPositionX() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getPlayerPositionX'");
+        int halfway;
+        if(end1X>=end0X){
+            halfway = (end1X-end0X)/2+end0X;
+            return halfway;
+        }
+        halfway = (end0X-end1X)/2+end1X;
+        return halfway;
     }
 
     @Override
     public int getPlayerPositionY() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getPlayerPositionY'");
+        int halfway;
+        if(end1Y>=end0Y){
+            halfway = (end1Y-end0Y)/2+end0Y;
+            return halfway;
+        }
+        halfway = (end0Y-end1Y)/2+end1Y;
+        return halfway;
     }
 }
